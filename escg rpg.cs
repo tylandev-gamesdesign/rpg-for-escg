af6b945d42ed9b207b4e521805bc5dac4e8fddb6
tylandev-gamesdesign <574136@escg.ac.uk>
2026-09-18 09:12:24 +0100

    Add files via upload

---
+++ escg rpg.cs
@@ -0,0 +1,49 @@
+﻿Console.WriteLine("write yor name for this amazing game that will knock your socks off.");
+string myName = Console.ReadLine();
+
+Console.WriteLine("Welcome to this amazing incredible lovely awesome phenomenal cool cooler coolest game, " + myName);
+Console.WriteLine("but hold your horses! you need to make party member names, silly billison. do the first one first because if you do the second one first i'd flip out.");
+string myName2 = Console.ReadLine();
+
+Console.WriteLine("FIRST new party member is.... " + myName2);
+Console.WriteLine("you should do the OTHER other party member, y'know.. THAT guy. theyre weird.");
+string myName3 = Console.ReadLine();
+
+Console.WriteLine("Welcome, i GUESS.. " + myName3);
+Console.WriteLine(@"       
+                                    *@@-              
+                                   .*@=@#          
+                                  :%@@@%@:         
+                                  .-.-@+@+          
+                                     =@:#@.     
+                                     %%.*@:           
+                                .:. =@= =@-           
+                              .#@@@-+@: =@-::.        
+                            .+@#..:@@@@@@@@@@.        
+                    -@@%@@%%@#-.    .....  :@%=..                     .-.                               .#%:       
+                     =@:     .:+%#+==+#%+:.  -#%%%@@.              .:*@@@=.                             ++.*+.     
+                     .*@=.  **:.        .:#+      +@            .+%@@+..=@@-                          .#+.  -%:    
+                      .-@%+*.              .#-    -@@*=.    :#@@#+:.  ::..+@%+:.                    .*+       -**:
+                        .%@%-:.              *+.  ..=@@:   .#@. ..+%*-  :=##-+%@@@#:..            .-#.    ..... .+@*=:.         
+                        -*::+@=%@@%%#.-%%*-:  *: .+@%:.    .#% .#-.         .*+ ..-#@*           .#-.  .*#=:::-##:  ..-%:         
+                        +:  .*#=..:.+%@=-+*##@@@@@*:       .#@=#.             -%..*@+.         -%*.  .#+.       .-%:  *+        
+                        *.   ...:-=+=  :----:.-*            =@*                :*@@:            .#-.:@:       .-.  #::%.      
+                        +:  .#@#*=-#@  +===@# =+            .@#                .#@:              .-%%: =%%.   *+%- :#:#          
+                        :#.       .@#     :@* #:             %# .:=*:-%-       .*-                .%% \@  #.  *  +/  %#         
+                         =#.       =.     .# %-              +@#@@#%@@@@-.:+%@=.#                :%:#.             .@.*+. 
+                          :#-              -#.               .#=:.            @%=               =#: =#             =+ .*=
+                            -#=.   .--:..=#-                  .%-          .##-           ..=#=    =*    #*+   .+*.   :#=
+                              .-@@%##%@@-                       -@=     V  ..#*.           .:@=........=@*.. :...=%*%=..  .+#..  
+                              .*@-.   =@-                         .-#%####%-.             .::--------:. .:*#*+=-%-  .-*#*+==%+ 
+                             .#@@:    .#@-.                        .@#   -@=                           .=%%.    =+  
+                            :@@*@:     .*@=                       *@@-   -@=                        .+@--%      .*
+                           =@* +@:      .*@.                    :@%%%:   :@@:                    .*#-.  #.      .*
+                          .%%. +@:       .#@:                 .-@#.%%.   .%@%.                .=%=.    -*.      .*.            
+                          -@=  +@:        -@+.               .*@+..%%.    =@@=      ..-@@@@@*@=.      :@:        ++             
+                          -@-  =@=        .@#.               *@@@=:@*     .#@@%*=:.:@@@@@@@@@:        +@##*=--::::.            
+                          :@#  -@+.        *%.              .+**:.:@+      =@=:=*##@@@@@@@@@%.
+                          .#@- .@*.        *@.                    :@+      -@=     .+*-.       
+                           .=@%@@@#: ......:@=                    :@%%@@@@%#=.                
+                            :@@@@@@%%%######@=                     ...     
+                            :@@@#:.            
+");
